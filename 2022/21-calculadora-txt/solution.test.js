4b58d195e8a69b47b7da7980bd8b71f1325dd2f8
marco cruz <marco24cruz08@gmail.com>
2023-01-12 14:56:52 -0600

    :sparkles: challenge-21 solution added

---
+++ 2022/21-calculadora-txt/solution.test.js
@@ -0,0 +1,33 @@
+const calculate = require('./solution');
+
+describe('Challenge 21: Calculadora TXT', () => {
+  const testCases = [
+    {
+      input: 'file-01',
+      output: 18.5,
+      description: 'should return 18.5',
+    },
+    {
+      input: 'file-02',
+      output: 8334.5,
+      description: 'should return 8334.5',
+    },
+    {
+      input: 'file-error',
+      output: 'Error: operators are not alternated',
+      description: 'should return "Error: operators are not alternated"',
+    },
+  ];
+
+  it('should return a number type if data is correct', () => {
+    expect(typeof calculate('file-01')).toBe('number');
+  });
+
+  it('should return a string type if data is incorrect', () => {
+    expect(typeof calculate('file-error')).toBe('string');
+  });
+
+  it.each(testCases)('$description', (testCase) => {
+    expect(calculate(testCase.input)).toBe(testCase.output);
+  });
+});
