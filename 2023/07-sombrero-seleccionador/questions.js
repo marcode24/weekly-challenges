1a319408e402bd3c0f79a6b6d695ea312b01084b
marco cruz <marco24cruz08@gmail.com>
2023-02-16 13:56:38 -0600

    :sparkles: challenge-07 solution added

---
+++ 2023/07-sombrero-seleccionador/questions.js
@@ -0,0 +1,101 @@
+const questions = [
+  {
+    question: 'Digamos que tiene un exámen la próxima semana, ¿como se va a preparar?',
+    answers: [
+      'Reviso mis apuntes y estudio solo en mi casa',
+      'Dejo todo para última hora y el día antes me trasnocho estudiando con uno '
+      + 'o dos amigos',
+      'Estudio un poquito cada día de la semana pero no me estreso porque sé que '
+      + 'he parado bolas en clase entonces me va a ir bien',
+      'Le digo a mis compañeros que nos reunamos a estudiar juntos en la biblioteca',
+    ],
+  },
+  {
+    question: 'Si su habitación en Hogwarts se está quemando y pudiera rescatar '
+    + 'solo una cosa, ¿cuál sería?',
+    answers: [
+      'Su libro favorito o su computador',
+      'Las fotos que tiene con sus amigos',
+      'Una reliquia familiar que ha pasado de generación en generación',
+      'A su mascota',
+    ],
+  },
+  {
+    question: '¿Cuál de estas cosas lo pone más nervioso?',
+    answers: [
+      'Hablar en público',
+      'Fracasar',
+      'Los espacios cerrados',
+      'La soledad',
+    ],
+  },
+  {
+    question: 'Si le dan un pedazo de papel, ¿qué hace con él?',
+    answers: [
+      'Dibuja',
+      'Escribe',
+      'Un avioncito de papel',
+      'Lo rompe en muchos pedacitos',
+    ],
+  },
+  {
+    question: 'Si en una clase los ponen a hacer un trabajo en grupo, ¿usted qué hace?',
+    answers: [
+      'Investiga y escribe gran parte del trabajo pero deja que alguien más en el'
+      + ' grupo lo decore y haga la presentación',
+      'Lo mínimo posible. En los grupos siempre hay alguien que se va a encargar '
+      + 'de hacerlo todo y eso es lo chévere',
+      'Hace de todo un poquito: ayuda con la investigación, organiza, escribe',
+      'Se pone a cargo de todo, organiza, asigna roles y termina casi haciéndolo '
+      + 'todo usted. Prefiere eso porque si no lo hace usted, no va a quedar bien hecho',
+    ],
+  },
+  {
+    question: '¿Haría trampa en un examen?',
+    answers: [
+      'Si ayudar a sus amigos que no estudiaron es hacer trampa, entonces sí',
+      'Pues, trata de no hacerlo siempre pero hay veces en las que toca',
+      'Nunca',
+      'Sí, todo el mundo hace trampa. De hecho, hacer trampa sin ser '
+      + 'pillado es muy inteligente',
+    ],
+  },
+  {
+    question: '¿Qué cosa le fastidia más?',
+    answers: [
+      'Que la gente se indigne por cualquier cosa',
+      'Que la gente no sea leal',
+      'Que la gente confunda ay, hay y ahí',
+      'Que la gente se aproveche de otra gente',
+    ],
+  },
+  {
+    question: 'Es fin de semana y está tratando de decidir dónde comer '
+    + 'con un amigo. ¿Qué hace?',
+    answers: [
+      'Busca restaurantes en google y le cuenta las mejores opciones que '
+      + 'encontró para que su amigo escoja',
+      'Le dice “vamos a ir a tal lado”. Es mejor que usted escoja de una '
+      + 'para evitar perder tiempo',
+      'Analizan cuál es el restaurante que más le gusta a los dos y van a ese',
+      'Le dice que vayan a probar algún sitio en el que ninguno de '
+      + 'los dos haya estado antes',
+    ],
+  },
+  {
+    question: 'Y por último, si un grupo de magos malvados atrapó a su mejor '
+    + 'amigo y lo tiene escondido en un sótano, ¿qué haría para rescatarlo?',
+    answers: [
+      'Conseguiría planos del sitio y excavaría un túnel. Puede que le tome '
+      + 'más de un mes hacerlo pero es la forma más segura y menos sospechosa '
+      + 'de recuperar a su amigo',
+      'Usaría una poción o un encantamiento para disfrazarse como alguno de '
+      + 'los captores y sacar a su amigo sin que nadie sospeche',
+      'Llamaría a alguno de los contactos de sus papás que tienen cargos '
+      + 'altos para que ellos planeen una misión de rescate',
+      'Reuniría a sus demás amigos y planearía una misión de rescate',
+    ],
+  },
+];
+
+module.exports = questions;
