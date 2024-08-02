3e389dfed0e7e2179b90b2b3f4ae922e029244fd
Marco Cruz <marco24cruz08@gmail.com>
2024-08-02 06:15:54 +0000

    :sparkles: Add solution challenge-13

---
+++ 2024/13-pruebas-unitarias/index.js
@@ -0,0 +1,13 @@
+const suma = (a, b) => a + b;
+
+const data = {
+  name: 'Marco Cruz',
+  age: new Date().getFullYear() - 2001,
+  birth_date: '2001-08-24',
+  programming_languages: ['JavaScript', 'TypeScript', 'C#', 'PHP'],
+};
+
+module.exports = {
+  suma,
+  data,
+};
